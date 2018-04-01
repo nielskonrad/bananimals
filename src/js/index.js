43e17d8e2ba0b50a32a64df7d16d71cbcb47e2c3
Niels Christian Konrad Nielsen <niels@rokoko.com>
2018-04-01 12:49:46 +0200

    Improved OO in animal class

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,29 @@
 // var trigger = document.querySelector('#o_shape');
 var banana = document.getElementById('banana');
 var bananaWrapper = document.querySelector('.banana-wrapper');
-var beakTop = document.getElementById('beak_top');
-var beakBot = document.getElementById('beak_bot');
-var tucanBody = document.getElementById('tucan_body');
-var tail = document.getElementById('tail');
 // var eye = document.getElementById('eye');
 
 var beakRect = null, beakIsOpen = false;
 
-function Bananimal (species, context, mouthU, mouthL, eye) {
+function Bananimal (species, context, body, mouthU, mouthL, eye, tongue) {
   this.species = species;
   this.context = context;
+  this.animalBody = body;
   this.mouthUpper = mouthU;
   this.mouthLower = mouthL;
   this.eye = eye;
+  this.tongueAnim = new TimelineMax();
+  this.tongueAnim.to(tongue.s, 1, { morphSVG: tongue.e, ease: Elastic.easeInOut.config(1, 0.3), shapeIndex:3, scale: 2 });
+  this.swallowAnim = new TimelineMax({paused:true});
+  this.swallowAnim.to("#slurp-1", 0.5, { morphSVG: "#slurp-2", ease: Sine.easeIn }).to("#slurp-1", 0.6, { morphSVG: "#slurp-3", ease: Sine.easeOut }).to("#slurp-1", 0.2, { morphSVG: "#slurp-1", ease: Sine.easeInOut });
   // Setup idle animation
-  function setupIdleAnimation(obj) {
+  this.setupIdleAnimation = function(obj) {
     var idleObj = obj;
+    var self = this;
+    function moveBody() {
+      TweenMax.to(self.animalBody, 1, {rotation:-4, repeat:-1, transformOrigin:"center", yoyo:true, ease: Power2.easeInOut});
+      TweenMax.to(self.animalBody.children[1], 0.8, {rotation:-6, repeat:-1, transformOrigin:"top right", yoyo:true, ease: Power2.easeInOut});
+    }
     function loopAnimation() {
       TweenLite.to(idleObj, 0.3, {x: -6, scale: 1.2, transformOrigin:"center"});
       TweenMax.staggerFrom(idleObj.children, 0.3, {x:-4}, 0.1);
@@ -33,47 +39,40 @@ function Bananimal (species, context, mouthU, mouthL, eye) {
       }, rand);
       // console.log(rand);
     }
+    moveBody();
     loopAnimation();
   }
-  this.tongue = function() {
-    var self = this;
-    self.anim = new TimelineMax({paused:true});
-    console.log('sdasdasd: ' + self.anim);
-    self.anim.to("#tongue", 1, { morphSVG: "#tongue_long", ease: Elastic.easeInOut.config(1, 0.3), scale: 2 });
+  // Swallow Animation
+  this.swallow = function() {
+    this.swallowAnim.play(0);
   }
-  // this.tongue = function() {
-    
-  // }
-  // var morphSwallow = new TimelineMax({paused:true});
-  // morphSwallow.to("#slurp-1", 0.5, { morphSVG: "#slurp-2", ease: Sine.easeIn })
-  //   .to("#slurp-1", 0.6, { morphSVG: "#slurp-3", ease: Sine.easeOut })
-  //   .to("#slurp-1", 0.2, { morphSVG: "#slurp-1", ease: Sine.easeInOut })
   // Open mouth
   this.affectMouth = function(beakOpen) {
-    console.log('hey');
+    // console.log('hey');
     var self = this;
     if (beakOpen) {
-      console.log(self.tongue);
-      // self.tongue.anim.play(0);
-      TweenLite.to(beakTop, 0.5, {rotation:-20, ease: Back.easeOut});
-      TweenLite.to(beakBot, 0.5, {rotation:20, ease: Back.easeOut});
+      // console.log(self.tongue);
+      this.tongueAnim.play(0);
+      TweenLite.to(this.mouthUpper, 0.5, {rotation:-20, ease: Back.easeOut});
+      TweenLite.to(this.mouthLower, 0.5, {rotation:20, ease: Back.easeOut});
       beakIsOpen = true;
     } else {
-      // self.tongue.anim.reverse(0);
-      TweenLite.to(beakTop, 0.5, {rotation:0, ease: Back.easeOut});
-      TweenLite.to(beakBot, 0.5, {rotation:0, ease: Back.easeOut});
+      this.tongueAnim.reverse(0);
+      TweenLite.to(this.mouthUpper, 0.5, {rotation:0, ease: Back.easeOut});
+      TweenLite.to(this.mouthLower, 0.5, {rotation:0, ease: Back.easeOut});
       // beakIsOpen = true;
     }
   }
-  this.tongue();
-  setupIdleAnimation(this.eye);
+  this.setupIdleAnimation(this.eye);
 }
 
-var tucan = new Bananimal('tucan',
+var animal = new Bananimal('tucan',
   document.getElementById('stick'),
+  document.getElementById('tucan_body'),
   document.getElementById('beak_top'),
   document.getElementById('beak_bot'),
-  document.getElementById('eye')
+  document.getElementById('eye'),
+  {s: document.getElementById('tongue'), e: document.getElementById('tongue_long')}
 );
 
 // var tucanTongue = document.getElementById('tongue');
@@ -100,23 +99,19 @@ bananaWrapper.addEventListener('mouseenter', function() {
 });
 
 bananaWrapper.addEventListener('mouseleave', function() {
-  console.log('leaving banana');
+  // console.log('leaving banana');
   TweenLite.to(this, 0.2, {scale: 1});
 });
 
 function setupBanana(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
   elmnt.onmousedown = dragMouseDown;
-
   getHoverArea();
-
   function getHoverArea() {
     // var beakRect = window.getComputedStyle(beakTop, null);
-    beakRect = beakTop.getBoundingClientRect();
+    beakRect = animal.mouthUpper.getBoundingClientRect();
     // console.log(beakRect);
   }
-
   function dragMouseDown(e) {
     e = e || window.event;
     // get the mouse cursor position at startup:
@@ -126,7 +121,6 @@ function setupBanana(elmnt) {
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
-
   function elementDrag(e) {
     e = e || window.event;
     // calculate the new cursor position:
@@ -137,20 +131,19 @@ function setupBanana(elmnt) {
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    console.log(e.clientX + ', ' + e.clientY);
+    // console.log(e.clientX + ', ' + e.clientY);
 
     if (e.clientX >= beakRect.left && e.clientX <= beakRect.right &&
       e.clientY >= beakRect.top && e.clientY <= beakRect.bottom) {
       // Mouse is inside element.
       // beakIsOpen = true;
-      tucan.affectMouth(true);
-      console.log(tucan);
+      animal.affectMouth(true);
+      // console.log(animal);
     } else {
       // beakIsOpen = false;
-      tucan.affectMouth(false);
+      animal.affectMouth(false);
     }
   }
-
   function closesetupBanana(e) {
     /* stop moving when mouse button is released:*/
     var target = e.srcElement || e.target;
@@ -174,8 +167,8 @@ function destroyFruit() {
   // Get editables position
   var fruitStyle = window.getComputedStyle(bananaWrapper, null);
   var fruitpos = {x: (parseInt(fruitStyle.left, 10)), y: (parseInt(fruitStyle.top, 10))};
-  console.log(fruitpos);
-  morphSwallow.play(0);
+  // console.log(fruitpos);
+  animal.swallow();
   // eatShape.style.left = '' + fruitpos.x + 'px';
   // eatShape.style.top = '' + fruitpos.y + 'px';
   TweenLite.to(bananaWrapper, .5, {scale:0, ease: Power2.easeInOut});
@@ -186,14 +179,13 @@ function destroyFruit() {
     // bananaWrapper.style.top = '' + fruitpos.y + 20 + 'px';
     // TweenLite.to(bananaWrapper, .5, {x: 200, y: -80, scale: 1, ease: Power2.easeInOut});
     // eatShape.parentNode.removeChild(eatShape);
-    affectMouth(false);
+    animal.affectMouth(false);
   }, 500);
 }
 
 setupBanana(bananaWrapper);
 
-TweenMax.to(tucanBody, 1, {rotation:-4, repeat:-1, transformOrigin:"center", yoyo:true, ease: Power2.easeInOut});
-TweenMax.to(tail, 0.8, {rotation:-6, repeat:-1, transformOrigin:"top right", yoyo:true, ease: Power2.easeInOut});
+
   
 
 // beakTop.addEventListener('mouseenter', function() {
